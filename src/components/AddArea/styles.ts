1c34e39b23a06f82c42e19949bcbaef50e4ddc70
Felipe Alves <115760676+felipenewplayer@users.noreply.github.com>
2023-01-31 14:52:15 -0300

    first commit

---
+++ src/components/AddArea/styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+        border-radius:15px;
+        border:1px solid #555;
+        padding:10px;
+        margin: 20px 0 ;
+        display:flex;
+        align-items: center;
+
+        .image {
+            margin-right:5px;
+        }
+        input{
+            width:100%;
+            background: transparent;
+            border:none;
+            outline:none;
+            color:#fff;
+            font-size:18px;
+            flex:1;
+        }
+`;
